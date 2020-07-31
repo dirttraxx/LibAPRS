f32e7df91dbfa3ecca596d1038f7ca7c97f40bb7
billygr <billygr@users.noreply.github.com>
2020-07-31 19:59:56 +0300

    Fix ifdef warning

--- device.h
+++ device.h
@@ -27,7 +27,7 @@
     #define LED_DDR  DDRB
 
 // Use Arduino digital pin 13 (PB5) for TX_PIN for Arduino Pro Mini and Arduino Nano
-#ifdef ARDUINO_AVR_PRO || ARDUINO_AVR_NANO
+#if defined(ARDUINO_AVR_PRO) || defined(ARDUINO_AVR_NANO)
     #define LED_RX_PIN 0
     #define LED_TX_PIN 5
 #else
